d7bac40eb209ccbd14c45f245b53dba1fc2a2167
webhazrat <mdhazrat445891@gmail.com>
2024-05-29 00:38:48 +0600

    basic operators and comparisions js added

---
+++ javascript/js/basic-operators-maths.js
@@ -0,0 +1,18 @@
+let a = "10";
+let b = "5";
+let c = 100;
+let d = 50;
+
+(BODMAS = brackets), of, division, multiplication, addition, substruction;
+
+const result = a / (b + (c + c) / d) + d;
+
+console.log(a ** b);
+
+let n = 5;
+
+n += 5;
+n *= 5;
+
+let counter = 1;
+counter++;
---
+++ javascript/js/comparisions.js
@@ -0,0 +1,7 @@
+console.log(5 < 2);
+console.log(5 <= 5);
+console.log(5 === 5); // == with type ===
+
+console.log(5 != "5");
+
+console.log(typeof undefined);
