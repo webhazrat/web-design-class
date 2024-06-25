e6e1f780cf7a3d3ad1d4ffade33788b51fbba0d0
webhazrat <mdhazrat445891@gmail.com>
2024-06-25 23:03:27 +0600

    New dom selector added

--- javascript/js/dom-selector.js
+++ javascript/js/dom-selector.js
@@ -15,3 +15,31 @@ qSelectorAll.forEach(function (el) {
   console.log(el);
 });
 // console.log(qSelectorAll);
+
+document.body; // select document body
+document.head; // select document head
+document.title; // select document title
+
+const delBtn = document.getElementById("delBtn");
+delBtn.style.color = "red";
+delBtn.style.background = "black";
+delBtn.style.border = "0";
+delBtn.style.borderRadius = "4px";
+console.log(delBtn.innerHTML);
+delBtn.innerHTML = "Another new text <p>Hello</p>";
+console.log(delBtn.classList);
+console.log(delBtn.innerText);
+// delBtn.innerText = "Another new text";
+delBtn.classList.add("new-class");
+// delBtn.classList.remove("new-class");
+console.log(delBtn.classList);
+delBtn.append("+");
+
+const list = document.querySelectorAll(".acc-list li");
+list.forEach(function (el) {
+  el.append("+");
+});
+
+const accList = document.querySelector(".acc-list");
+const parent = accList.closest("div");
+console.log(parent);
